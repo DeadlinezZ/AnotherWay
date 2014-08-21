191c155b95c6c33ef125f36018668a5ac7020ec6
DeadlinezZ <fabrice-95@hotmail.com>
2014-08-21 11:38:53 +0200

    Added deleteBuilds Batch

---
+++ deleteBuilds.bat
@@ -0,0 +1,9 @@
+RMDIR "%~dp0client\AnotherWayClient\Builds\64" /S /Q
+MKDIR "%~dp0client\AnotherWayClient\Builds\64"
+RMDIR "%~dp0client\AnotherWayClient\Builds\32" /S /Q
+MKDIR "%~dp0client\AnotherWayClient\Builds\32"
+RMDIR "%~dp0server\AnotherWayServer\Builds\64" /S /Q
+MKDIR "%~dp0server\AnotherWayServer\Builds\64"
+RMDIR "%~dp0server\AnotherWayServer\Builds\32" /S /Q
+MKDIR "%~dp0server\AnotherWayServer\Builds\32"
+exit
