79948290637484f22f123a894ca058fe08fe69c8
lamune17 <storm-earth-end-fire@hotmail.com>
2014-08-23 13:46:20 +0200

    Modell und Scripts
    hinzugefügt

---
+++ client/AnotherWayClient/Assets/scripts/Character/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Camera playerCamera;
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 1.2F;
+	public float yOffset = 4.0f;
+	public float zOffset = 4.0f;
+	
+	private CharacterController controller;
+	private Vector3 offset;
+	
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		offset = new Vector3(controller.transform.position.x,controller.transform.position.y + yOffset,controller.transform.position.z + zOffset);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+		float v = verticalSpeed * Input.GetAxis("Mouse Y");
+		offset = Quaternion.AngleAxis(h,Vector3.up) * offset;
+		offset = Quaternion.AngleAxis(v,Vector3.right) * offset;
+		
+		playerCamera.transform.position = controller.transform.position + offset;
+		playerCamera.transform.LookAt(controller.transform.position);
+		
+		
+		
+	}
+}
---
+++ client/AnotherWayClient/Assets/scripts/Character/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private CharacterController controller;
+	private Animator animator;
+	public Camera playerCamera;
+	public int speed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
+
+	//TODO
+	//Animator braucht noch speed value
+	//Aufeinmal funtioniert das laufen nicht mehr.
+	//Von Legacy auf Humanoid gewechselt. ?=? sollte aber keinen Einfluss haben.
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Quaternion qa = new Quaternion(0,playerCamera.transform.rotation.y,0,playerCamera.transform.rotation.w);
+		
+
+		if(Input.GetKey(KeyCode.W)){
+			Vector3 w = playerCamera.transform.TransformDirection(Vector3.forward);
+			controller.SimpleMove(w * speed);
+			
+		}else if(Input.GetKey(KeyCode.S)) {
+			Vector3 s = playerCamera.transform.TransformDirection(Vector3.back);
+			controller.SimpleMove(s * speed);
+
+			
+		}else if(Input.GetKey(KeyCode.D)) {
+			Vector3 d = playerCamera.transform.TransformDirection(Vector3.right);
+			controller.SimpleMove(d * speed);
+			
+		}else if(Input.GetKey(KeyCode.A)) {
+			Vector3 a = playerCamera.transform.TransformDirection(Vector3.left);
+			controller.SimpleMove(a * speed);
+			
+		}
+		//Rotates the player
+		transform.rotation = qa;
+	}
+}
